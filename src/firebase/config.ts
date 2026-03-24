700efac2c2a72543b580c6efcec4def558a6290a
Basim althobaiti <gjafagifts@gmail.com>
2026-03-24 04:05:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-85890230-a9093",
+  "appId": "1:1064930272521:web:d53d67f908ad719da0dfd8",
+  "apiKey": "AIzaSyD6pvF-1gSzBFs332qKoVM1MDbRvtIF94c",
+  "authDomain": "studio-85890230-a9093.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1064930272521"
+};
